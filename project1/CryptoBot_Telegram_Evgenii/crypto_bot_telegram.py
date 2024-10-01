7c7938fc37097b134d09ba160ba68a4aba2be57d
Evgeniy <tuq_solo@mail.ru>
2024-10-01 12:12:26 +0300

    crypto tg bot

--- project1/CryptoBot_Telegram_Evgenii/crypto_bot_telegram.py
+++ project1/CryptoBot_Telegram_Evgenii/crypto_bot_telegram.py
@@ -49,10 +49,11 @@ def convert(message: telebot.types.Message):
                 available_commands = [str(command) for command in commands_list]
                 raise ConvertionException(f'Команда {start_values} недоступна.\n'
                                           f'Список доступных команд: {', '.join(available_commands)}')
-        count_values = message.text.split(' ')
-
-        if len(count_values) != 3:
-            raise ConvertionException(f'Некорректное кол-во параметров (требуются 3). У вас их {len(count_values)}.')
+        count_values = message.text.split()
+        if ' ' in count_values:
+            count_values = [value for value in count_values.split() if value.strip()]
+        elif len(count_values) != 3:
+            raise ConvertionException(f'Некорректное кол-во параметров (требуются 3). У вас их {len(count_values)}')
 
         quote, base, amount = count_values
         total_base = CryptoConverter.converter(quote, base, amount)
