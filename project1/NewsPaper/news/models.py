05340caeb6640ce1b2cdf5990024730241078c5e
Evgeniy <tuq_solo@mail.ru>
2024-11-15 13:31:00 +0300

    commit

--- project1/NewsPaper/news/models.py
+++ project1/NewsPaper/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime, timezone
 
 director = 'DI'
 admin = 'AD'
@@ -27,6 +28,9 @@ class Staff(models.Model):
                                 default=cashier)
     labor_contract = models.IntegerField()
 
+    def get_last_name(self):
+        return self.full_name.split()[0]
+
 
 class Order(models.Model):
     time_in = models.DateTimeField(auto_now_add=True)
@@ -37,8 +41,42 @@ class Order(models.Model):
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='ProductOrder')
 
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:  # если завершён, возвращаем разность объектов
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:  # если ещё нет, то сколько длится выполнение
+            return (datetime.now(timezone.utc) - self.time_in).total_seconds() // 60
+
 
 class ProductOrder(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
+    class ProductOrder(models.Model):
+        product = models.ForeignKey(Product, on_delete=models.CASCADE)
+        order = models.ForeignKey(Order, on_delete=models.CASCADE)
+        _amount = models.IntegerField(default=1, db_column='amount')
+
+        @property
+        def amount(self):
+            return self._amount
+
+        @amount.setter
+        def amount(self, value):
+            self._amount = int(value) if value >= 0 else 0
+            self.save()
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self.amount
+
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=64)
+    name = models.CharField(null=True, max_length=64)
+
+    def some_method(self):
+        self.name = self.full_name.split()[0]
+        self.save()
