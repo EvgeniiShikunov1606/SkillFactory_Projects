83e5d25826c560efa9a22349be23ebc6066c603a
Evgeniy <tuq_solo@mail.ru>
2025-01-30 13:29:01 +0300

    commit

--- project1/NewsPaper/news/views.py
+++ project1/NewsPaper/news/views.py
@@ -34,13 +34,11 @@ class PostsList(ListView):
     paginate_by = 10
     filterset = None
 
-    # Возвращаем фильтрованный queryset
     def get_queryset(self):
         queryset = super().get_queryset()
         self.filterset = PostFilter(self.request.GET, queryset)
         return self.filterset.qs
 
-    # Настраиваем контекстные данные
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filterset'] = self.filterset
