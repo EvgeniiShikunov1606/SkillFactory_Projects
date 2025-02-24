a150913ee65397808da710b3ec16123cd5e87f8f
Evgeniy <tuq_solo@mail.ru>
2025-02-24 12:19:27 +0300

    create User and Pass modules. Added 'pass' to INSTALLED_APPS

--- project1/PassApp/pass/models.py
+++ project1/PassApp/pass/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    fam = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    otc = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name} {self.fam}{f' {self.otc}' if self.otc else ''}"
+
+
+class Pass(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected')
+    ]
+
+    beauty_title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    other_titles = models.TextField(blank=True, null=True)
+    connect = models.TextField(blank=True, null=True)
+
+    add_time = models.DateTimeField()
+    user = models.ForeignKey(User, related_name='pass', on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.PositiveIntegerField()
+
+    level_winter = models.CharField(max_length=10, blank=True, null=True)
+    level_summer = models.CharField(max_length=10, blank=True, null=True)
+    level_autumn = models.CharField(max_length=10, blank=True, null=True)
+    level_spring = models.CharField(max_length=10, blank=True, null=True)
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='new')
+
+    def __str__(self):
+        return f"{self.title} ({self.status})"
