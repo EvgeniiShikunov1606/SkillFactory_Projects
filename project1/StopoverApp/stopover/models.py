15ec585a27e8ed9d1fd2938688161f951743a004
Evgeniy <tuq_solo@mail.ru>
2025-02-24 16:10:25 +0300

    Changed Database. Added StopoverLevel and StopoverCoords models. Migration are made

--- project1/StopoverApp/stopover/models.py
+++ project1/StopoverApp/stopover/models.py
@@ -12,6 +12,19 @@ class User(models.Model):
         return f"{self.name} {self.fam}{f' {self.otc}' if self.otc else ''}"
 
 
+class StopoverLevel(models.Model):
+    level_winter = models.CharField(max_length=10, blank=True, null=True)
+    level_summer = models.CharField(max_length=10, blank=True, null=True)
+    level_autumn = models.CharField(max_length=10, blank=True, null=True)
+    level_spring = models.CharField(max_length=10, blank=True, null=True)
+
+
+class StopoverCoords(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.PositiveIntegerField()
+
+
 class Stopover(models.Model):
     STATUS_CHOICES = [
         ('new', 'New'),
@@ -25,16 +38,16 @@ class Stopover(models.Model):
     other_titles = models.TextField(blank=True, null=True)
     connect = models.TextField(blank=True, null=True)
 
-    add_time = models.DateTimeField()
+    add_time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, related_name='stopover', on_delete=models.CASCADE)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    height = models.PositiveIntegerField()
 
-    level_winter = models.CharField(max_length=10, blank=True, null=True)
-    level_summer = models.CharField(max_length=10, blank=True, null=True)
-    level_autumn = models.CharField(max_length=10, blank=True, null=True)
-    level_spring = models.CharField(max_length=10, blank=True, null=True)
+    coords = models.ForeignKey(
+        StopoverCoords,
+        related_name='stopover_coords',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True)
+    level = models.ForeignKey(StopoverLevel, related_name='stopover_level', on_delete=models.CASCADE)
 
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='new')
 
@@ -43,9 +56,9 @@ class Stopover(models.Model):
 
 
 class StopoverImage(models.Model):
-    stopover_obj = models.ForeignKey(Stopover, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='media/stopover_images/')
+    stopover = models.ForeignKey(Stopover, related_name='images', on_delete=models.CASCADE)
+    data = models.TextField()
     title = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.title} ({self.stopover_obj.title})"
+        return f"{self.title} ({self.stopover.title})"
